0952cb64635540d1cb6797471d14c3b84cfabc6f
Olivier <olivierdautricourt@gmail.com>
2016-04-08 21:38:10 +0200

    remove dmz dependancy

--- hackintn/settings.py
+++ hackintn/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #'rest_framework',
     'core.apps.CoreConfig',
-    'dmz.apps.DmzConfig',
+    #'dmz.apps.DmzConfig',
     'revproxy',
 ]
 
