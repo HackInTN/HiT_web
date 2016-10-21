3f035e9fb20c7d884809e49f5a3a2714665326b1
Olivier <olivierdautricourt@gmail.com>
2016-10-22 00:13:08 +0200

    fix indent / enhance flag str / supress make clean output

--- core/flag_validation.py
+++ core/flag_validation.py
@@ -4,12 +4,13 @@ import subprocess
 def crypt_b64(validation_key, flag):
     if len(validation_key) < len(flag):
         return
-    return base64.b64encode(bytes([a^b for a,b in zip(validation_key, flag)]))
+    return bytes(flag) + b'_' + \
+           base64.b64encode(bytes([a^b for a,b in zip(validation_key, flag)]))
 
 def make_bin(user, challenge):
     path = "challenges/bin/" + challenge.name
     encflag = challenge.getEncryptedFlag(user)
-    binary = subprocess.check_output("make --silent -C{} _FLAG_={} _ID_={}" \
+    binary = subprocess.check_output("make -s -C{} _FLAG_={} _ID_={}" \
                    .format(path, encflag.decode(), user.pk), shell=True)
-    subprocess.run("make clean -C" + path, shell=True)
+    subprocess.run("make -s clean -C" + path, shell=True)
     return binary
